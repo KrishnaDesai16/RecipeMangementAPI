7e79e79af40a94b5b9a2bc5cc713d783f32849e0
Krishna Desai <58232684+KrishnaDesai16@users.noreply.github.com>
2021-04-14 21:21:45 +0530

    code added

---
+++ app.py
@@ -0,0 +1,135 @@
+"""REST API app using "Flask + SQLAlchemy" with marshmallow. """
+
+from flask import Flask,jsonify,request,make_response
+from http import HTTPStatus
+from flask_sqlalchemy import SQLAlchemy  #thorugh python change is databse
+from marshmallow import fields,ValidationError #handle ValidationError
+from marshmallow_sqlalchemy import ModelSchema
+
+### ERRORS HANDLING ##
+def page_not_found(e):  # error:URL Not Found
+    return jsonify({'message': 'URL not found !!'}), HTTPStatus.NOT_FOUND
+def BAD_REQUEST(e): #errpr: check syntax error, Invalid Request message
+    return jsonify({'message': 'BAD REQUEST !! Syntax,Invalid Request Message Framing,Or Deceptive Request Routing'}),HTTPStatus.BAD_REQUEST
+def method_not_allowed(e): # error:when you pass wrong url
+    return jsonify({'message': 'Method Not Allowed !!'}), HTTPStatus.METHOD_NOT_ALLOWED
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///recipe.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+app.register_error_handler(404, page_not_found)
+app.register_error_handler(400,BAD_REQUEST)
+app.register_error_handler(405,method_not_allowed)
+db=SQLAlchemy(app)
+
+
+##### MODELS #####
+
+class recipe(db.Model):
+    Recipe_ID=db.Column(db.Integer,primary_key=True)
+    Recipe=db.Column(db.String(500),nullable=False)
+    Dish=db.Column(db.String(20),nullable=False)
+
+    def create(self):
+       db.session.add(self)
+       db.session.commit()
+       return self
+
+    def __init__(self,Dish,Recipe):
+            self.Dish = Dish
+            self.Recipe=Recipe
+
+    def __repr__(self):
+                return f"{self.Recipe_ID}"
+
+### SCHEMAS ###
+
+# Custom validator
+def must_not_be_blank(data):
+    if not data:
+        raise ValidationError("Recipe not created.") #raise Validation error of input data has different datatype
+
+class recipeSchema(ModelSchema):
+      class Meta(ModelSchema.Meta):
+           model = recipe
+           sqla_session = db.session
+      Recipe_ID = fields.Integer(dump_only=True)
+      Recipe = fields.String(required=True,validate=must_not_be_blank)  #custom error
+      Dish = fields.String(required=True,validate=must_not_be_blank)  #custom error
+
+##### API #####
+
+# Get All Recipes    
+@app.route('/recipes', methods=['GET'])
+def get_recipes():
+   
+   get_all = recipe.query.all()
+   recipe_schema = recipeSchema(many=True)
+   recipes = recipe_schema.dump(get_all)
+   if recipes:
+      return make_response(jsonify({"Recipes": recipes}))
+   return jsonify({'message': 'recipes not found'}), HTTPStatus.NOT_FOUND
+
+# Get All Recipes By ID
+@app.route('/recipes/<int:Recipe_ID>', methods=['GET'])
+def get_recipe_by_id(Recipe_ID):
+   get_recipe = recipe.query.get(Recipe_ID)
+   recipe_schema = recipeSchema()
+   recipes = recipe_schema.dump(get_recipe)
+   if recipes:
+          return make_response(jsonify({"Recipe": recipes}))
+   return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
+
+#Add Recipe 
+@app.route('/recipes', methods=['POST'])
+def create_recipe():
+   data = request.get_json()
+   if not data:
+        return {"message": "No input data provided"},400 #error:data is not in json format
+   recipe_schema = recipeSchema()
+   try:
+      recipes = recipe_schema.load(data)
+   except ValidationError as err:
+        return err.messages, 422    #error: if input data is not in correct datatype
+   result = recipe_schema.dump(recipes.create())
+   return make_response(jsonify({"Recipe": result})),HTTPStatus.CREATED
+   
+
+#Update Recipe
+@app.route('/recipes/<int:Recipe_ID>', methods=['PUT'])
+def update_receipe(Recipe_ID):
+      data=request.get_json()
+      if not data:
+        return {"message": "No input data provided"},400  #error:data is not in json format
+      get_recipe=recipe.query.get(Recipe_ID)
+      print(get_recipe)
+      if data.get('Recipe'):
+         get_recipe.Recipe = data['Recipe']
+      if data.get('Dish'):
+         get_recipe.Dish = data['Dish']
+      db.session.add(get_recipe)
+      db.session.commit()
+      recipe_schema = recipeSchema(only=['Recipe_ID', 'Recipe', 'Dish'])
+      recipes = recipe_schema.dump(get_recipe)
+      if not recipes:
+         return jsonify({'message': 'recipe not found'}),HTTPStatus.NOT_FOUND 
+      return make_response(jsonify({"Recipe": recipes}))
+   
+   
+#Delete Recipe By ID
+@app.route('/recipes/<int:Recipe_ID>', methods=['DELETE'])
+def delete_recipe_by_id(Recipe_ID):
+   get_recipe = recipe.query.get(Recipe_ID)
+   if get_recipe:
+      db.session.delete(get_recipe)
+      db.session.commit()
+      return make_response("", 204) # recipe deleted sucessfully
+   return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND  #error:if recipe not found in database
+
+  
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
