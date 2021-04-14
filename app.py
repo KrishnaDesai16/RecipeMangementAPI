5bd8508138b1dcdb092a2337aa87fa1657a0ae3b
Krishna Desai <58232684+KrishnaDesai16@users.noreply.github.com>
2021-04-15 03:41:06 +0530

    code added

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def BAD_REQUEST(e): #errpr: check syntax error, Invalid Request message
 def method_not_allowed(e): # error:when you pass wrong url
     return jsonify({'message': 'Method Not Allowed !!'}), HTTPStatus.METHOD_NOT_ALLOWED
 
+### DATABASE DEFINATION ###
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///recipe.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
@@ -25,7 +26,6 @@ db=SQLAlchemy(app)
 
 
 ##### MODELS #####
-
 class recipe(db.Model):
     Recipe_ID=db.Column(db.Integer,primary_key=True)
     Recipe=db.Column(db.String(500),nullable=False)
@@ -43,13 +43,43 @@ class recipe(db.Model):
     def __repr__(self):
                 return f"{self.Recipe_ID}"
 
-### SCHEMAS ###
 
-# Custom validator
+### Custom validator ###
+class InvalidDataType(Exception):
+       pass
+
 def must_not_be_blank(data):
     if not data:
-        raise ValidationError("Recipe not created.") #raise Validation error of input data has different datatype
+        raise ValidationError("Can't Empty!") #raise Validation error of input data has different datatype
 
+def null_and_type_check(data, recipeObject):
+   messageString = []
+   emptyVariables = []   
+   if data.get('Recipe'):
+      recipeObject.Recipe = data['Recipe']
+      if type(recipeObject.Recipe)!=str:
+         messageString.append("Invalid data type: Recipe needs to be String")
+      if type(recipeObject.Recipe)==str and data.get('Recipe').strip() == '':
+         emptyVariables.append("Error: Recipe cannot be empty")
+   else:
+      emptyVariables.append("Error: Recipe cannot be empty")
+
+   if data.get('Dish'):
+      recipeObject.Dish = data['Dish']
+      if type(recipeObject.Dish)!=str or (recipeObject.Dish==''):
+         messageString.append(" Invalid data type: Dish needs to be String")
+      if  type(recipeObject.Dish)==str and data.get('Dish').strip() == '' : 
+         emptyVariables.append("Error: Dish cannot be empty")
+   else:
+      emptyVariables.append("Error: Dish cannot be empty")
+   output = emptyVariables + messageString
+   if output:
+      return ', '.join(output)
+   else:
+      return '' 
+
+
+### SCHEMAS ###
 class recipeSchema(ModelSchema):
       class Meta(ModelSchema.Meta):
            model = recipe
@@ -59,7 +89,6 @@ class recipeSchema(ModelSchema):
       Dish = fields.String(required=True,validate=must_not_be_blank)  #custom error
 
 ##### API #####
-
 # Get All Recipes    
 @app.route('/recipes', methods=['GET'])
 def get_recipes():
@@ -92,10 +121,10 @@ def create_recipe():
       recipes = recipe_schema.load(data)
    except ValidationError as err:
         return err.messages, 422    #error: if input data is not in correct datatype
+      
    result = recipe_schema.dump(recipes.create())
    return make_response(jsonify({"Recipe": result})),HTTPStatus.CREATED
    
-
 #Update Recipe
 @app.route('/recipes/<int:Recipe_ID>', methods=['PUT'])
 def update_receipe(Recipe_ID):
@@ -105,17 +134,16 @@ def update_receipe(Recipe_ID):
       get_recipe=recipe.query.get(Recipe_ID)
       if(get_recipe == None):
          return {"message": "Recipe Id doesn't exist, can't update!"}, 404
-      if data.get('Recipe'):
-         get_recipe.Recipe = data['Recipe']
-         print(get_recipe.Recipe)
-      if data.get('Dish'):
-         get_recipe.Dish = data['Dish']
+      improperData = null_and_type_check(data, get_recipe)
+      if improperData:
+            return {"message": improperData}, 404
+
       db.session.add(get_recipe)
       db.session.commit()
       recipe_schema = recipeSchema(only=['Recipe_ID', 'Recipe', 'Dish'])
       recipes = recipe_schema.dump(get_recipe)
       if recipes:
-          return make_response(jsonify({"Recipe": recipes})),HTTPStatus.CREATED
+          return make_response(jsonify({"Recipe": recipes})),HTTPStatus.OK
       return jsonify({'message': 'recipe not found'}),HTTPStatus.NOT_FOUND 
      
    
@@ -136,9 +164,3 @@ def delete_recipe_by_id(Recipe_ID):
 
 if __name__=="__main__":
     app.run(debug=True)
-
-
-
-person = Person.query \
-        .filter(Person.person_id == person_id) \
-        .one_or_none()
